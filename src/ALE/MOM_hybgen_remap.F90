ee2d7850178d5d5fa6acd42625fd2e044a796bb8
Robert Hallberg <Robert.Hallberg@noaa.gov>
2022-02-17 15:05:31 -0500

    (*)Make hybgen_weno_coefs dimensionally consistent
      Eliminated the hard-coded dimensional constant dsmll from hybgen_weno_coefs,
    to prevent obvious dimensional inconsistency.  Also refactored hybgen_ppm_coefs
    and hybgen_weno_coefs for more vertical symmetry and for clarity.  This change
    will change answers in cases that use the WENO_HYBGEN remapping scheme.

--- src/ALE/MOM_hybgen_remap.F90
+++ src/ALE/MOM_hybgen_remap.F90
@@ -290,9 +290,11 @@ subroutine hybgen_plm_coefs(si, dpi, slope, nk, ns, thin, PCM_lay)
 !     Alan J. Wallcraft,  Naval Research Laboratory,  Aug. 2007.
 !-----------------------------------------------------------------------
 !
+  real :: qcen   ! A layer's thickness divided by the distance between the centers
+                 ! of the adjacent cells, usually ~0.5, but always <= 1 [nondim]
+  real :: zbot, zcen, ztop ! Tracer slopes times the layer thickness [A]
   integer :: i, k
-  real :: qcen,zbot,zcen,ztop
-!
+
   do i=1,ns
     slope(1, i) = 0.0
     slope(nk,i) = 0.0
@@ -302,7 +304,7 @@ subroutine hybgen_plm_coefs(si, dpi, slope, nk, ns, thin, PCM_lay)
       do i=1,ns ; slope(k,i) = 0.0 ; enddo
     else
 ! ---     use qcen in place of 0.5 to allow for non-uniform grid
-      qcen = dpi(k)/(dpi(k)+0.5*(dpi(k-1)+dpi(k+1)))  !dpi(k)>thin
+      qcen = dpi(k) / (dpi(k)+0.5*(dpi(k-1)+dpi(k+1)))  !dpi(k)>thin
       do i=1,ns
 ! ---       PLM (non-zero slope, but no new extrema)
 ! ---       layer value is si-0.5*slope at top    interface,
@@ -315,7 +317,7 @@ subroutine hybgen_plm_coefs(si, dpi, slope, nk, ns, thin, PCM_lay)
         zbot = 2.0*(si(k+1,i)-si(k,  i))
         zcen = qcen*(si(k+1,i)-si(k-1,i))
         if     (ztop*zbot > 0.0) then !ztop,zbot are the same sign
-          slope(k,i) = sign(min(abs(zcen),abs(zbot),abs(ztop)),zbot)
+          slope(k,i) = sign(min(abs(zcen),abs(zbot),abs(ztop)), zbot)
         else
           slope(k,i) = 0.0  !local extrema, so no slope
         endif
@@ -511,10 +513,12 @@ subroutine hybgen_ppm_coefs(s, h_src, edges, nk, ns, thin, PCM_lay)
   real :: sck       ! Scalar differences across a cell.
   real :: as(nk)    ! Scalar field difference across each cell [A]
   real :: al(nk), ar(nk)   ! Scalar field at the left and right edges of a cell [A]
-  real :: dpkp(nk), dp2kp(nk), dpk2p(nk)  ! Combinations of thicknesses [H ~> m or kg m-2]
-  real :: qdpkp(nk), qdp2kp(nk), qdpk2p(nk) ! Inverses of combinations of thicnesses [H-1 ~> m-1 or m2 kg-1]
-  real :: dpq3(nk)  ! A ratio of a layer thickness of the sum of 3 adjacent thicknesses [nondim]
-  real :: qdp4(nk)  ! Inverse of the sum of 4 adkacent thicknesses [H ~> m or kg m-2]
+  real :: h112(nk+1), h122(nk+1)  ! Combinations of thicknesses [H ~> m or kg m-2]
+  real :: I_h12(nk+1) ! Inverses of combinations of thickesses [H-1 ~> m-1 or m2 kg-1]
+  real :: h2_h123(nk)  ! A ratio of a layer thickness of the sum of 3 adjacent thicknesses [nondim]
+  real :: I_h0123(nk)     ! Inverse of the sum of 4 adjacent thicknesses [H-1 ~> m-1 or m2 kg-1]
+  real :: h01_h112(nk+1) ! A ratio of sums of adjacent thicknesses [nondim], 2/3 in the limit of uniform thicknesses.
+  real :: h23_h122(nk+1) ! A ratio of sums of adjacent thicknesses [nondim], 2/3 in the limit of uniform thicknesses.
   integer :: k, i
 
   ! This PPM remapper is not currently written to work with massless layers, so set
@@ -529,19 +533,20 @@ subroutine hybgen_ppm_coefs(s, h_src, edges, nk, ns, thin, PCM_lay)
   endif
 
   !compute grid metrics
-  do k=1,nk-1
-    dpkp( k) = dp(k)   + dp(k+1)
-    dp2kp(k) = dp(k)   + dpkp(k) ! = 2*dp(k) + dp(k+1)
-    dpk2p(k) = dpkp(k) + dp(k+1) ! = dp(k) + 2*dp(k+1)
-    qdpkp( k) = 1.0/dpkp( k)
-    qdp2kp(k) = 1.0/dp2kp(k)
-    qdpk2p(k) = 1.0/dpk2p(k)
+  do k=2,nk
+    h112(K) = 2.*dp(k-1) + dp(k)
+    h122(K) = dp(k-1) + 2.*dp(k)
+    I_h12(K) = 1.0 / (dp(k-1) + dp(k))
   enddo !k
-    dpq3(2) = dp(2)/(dp(1)+dpkp(2))
+  do k=2,nk-1
+    h2_h123(k) = dp(k) / (dp(k) + (dp(k-1)+dp(k+1)))
+  enddo
   do K=3,nk-1
-    dpq3(k) = dp(k)/(dp(k-1)+dpkp(k)) ! = dp(k) / (dp(k-1)+dp(k)+dp(k+1))
-    qdp4(K) = 1.0/(dpkp(k-2)+dpkp(k)) ! = 1.0 / (dp(k-2)+dp(k-1)+dp(k)+dp(k+1))
-  enddo !k
+    I_h0123(K) = 1.0 / ((dp(k-2) + dp(k-1)) + (dp(k) + dp(k+1)))
+
+    h01_h112(K) = (dp(k-2) + dp(k-1)) / (2.0*dp(k-1) + dp(k))
+    h23_h122(K) = (dp(k) + dp(k+1))   / (dp(k-1) + 2.0*dp(k))
+  enddo
 
   do i=1,ns
     !Compute average slopes: Colella, Eq. (1.8)
@@ -553,7 +558,7 @@ subroutine hybgen_ppm_coefs(s, h_src, edges, nk, ns, thin, PCM_lay)
         slk = s(k,  i)-s(k-1,i)
         srk = s(k+1,i)-s(k,  i)
         if (slk*srk > 0.) then
-          sck = dpq3(k)*( dp2kp(k-1)*srk*qdpkp(k) + dpk2p(k)*slk*qdpkp(k-1) )
+          sck = h2_h123(k)*( h112(K)*srk*I_h12(K+1) + h122(K+1)*slk*I_h12(K) )
           as(k) = sign(min(abs(2.0*slk), abs(sck), abs(2.0*srk)), sck)
         else
           as(k) = 0.
@@ -567,14 +572,11 @@ subroutine hybgen_ppm_coefs(s, h_src, edges, nk, ns, thin, PCM_lay)
     al(2) = s(1,i)  ! 1st layer PCM
     do K=3,nk-1
       ! This is a 4th order explicit edge value estimate.
-      al(k) = s(k-1,i) + dp(k-1)*(s(k,i)-s(k-1,i))*qdpkp(k-1) &
-            + qdp4(K)*( &
-              2.*dp(k)*dp(k-1)*qdpkp(k-1)*(s(k,i)-s(k-1,i))* &
-              ( dpkp(k-2)*qdp2kp(k-1) - dpkp(k) *qdpk2p(k-1) ) &
-              - dp(k-1)*as(k)  *dpkp(k-2)*qdp2kp(k-1) &
-              + dp(k)  *as(k-1)*dpkp(k)  *qdpk2p(k-1) &
-                )
-      ar(k-1) = al(k)
+      al(k) = (dp(k)*s(k-1,i) + dp(k-1)*s(k,i)) * I_h12(K) &
+            + I_h0123(K)*( 2.*dp(k)*dp(k-1)*I_h12(K)*(s(k,i)-s(k-1,i)) * &
+                           ( h01_h112(K) - h23_h122(K) ) &
+                    + (dp(k)*as(k-1)*h23_h122(K) - dp(k-1)*as(k)*h01_h112(K)) )
+     ar(k-1) = al(k)
     enddo !k
     ar(nk-1) = s(nk,i) ! last layer PCM
     al(nk)  = s(nk,i)  ! last layer PCM
@@ -621,7 +623,9 @@ subroutine hybgen_weno_coefs(s, h_src, edges, nk, ns, thin, PCM_lay)
 !             using a WENO reconciliation of the slopes to compute the
 !             interfacial values
 !
-!     REFERENCE?
+!     This scheme might have ben developed by Shchepetkin. A.F., personal communication.
+!     See also Engwirda, D., and M. Kelley, A WENO-type slope-limiter for a family of piecewise
+!       polynomial methods, arXive:1606.08188v1, 27 June 2016.
 !
 !  2) input arguments:
 !       s     - initial scalar fields in pi-layer space
@@ -640,10 +644,11 @@ subroutine hybgen_weno_coefs(s, h_src, edges, nk, ns, thin, PCM_lay)
 !     Alan J. Wallcraft,  Naval Research Laboratory,  July 2008.
 !-----------------------------------------------------------------------
 !
-  real, parameter :: dsmll=1.0e-8  !### This has units of [A2], and hence can not be a parameter.
+!  real, parameter :: dsmll=1.0e-8  ! This has units of [A2], and hence can not be a parameter.
 !
   real :: curv_cell   ! An estimate of the tracer curvature centered on a cell times the grid
                       ! spacing [A H-1 ~> A m-1 or A kg m-2]
+  real :: seh1, seh2  ! Tracer slopes at the cell edges times the cell grid spacing [A]
   real :: q01, q02    ! Various tracer differences between a cell average and the edge values [A]
   real :: q001, q002  ! Tracer slopes at the cell edges times the cell grid spacing [A]
   real :: ds2a, ds2b  ! Squared tracer differences between a cell average and the edge values [A2]
@@ -653,10 +658,15 @@ subroutine hybgen_weno_coefs(s, h_src, edges, nk, ns, thin, PCM_lay)
   real :: qdpkm(nk)   ! Inverse of the sum of two adjacent thicknesses [H-1 ~> m-1 or m2 kg-1]
   real :: qdpkmkp(nk) ! Inverse of the sum of three adjacent thicknesses [H-1 ~> m-1 or m2 kg-1]
   real :: dpkm2kp(nk) ! Twice the distance between the centers of the layers two apart [H ~> m or kg m-2]
-  real :: zw(nk,2)    ! Squared tracer differences between [A2 H-2 ~> A2 m-2 or A2 kg2 m-4]
+  real :: zw(nk,2)    ! Squared combinations of the differences between the the cell average tracer
+                      ! concentrations and the left and right edges [A2]
+  real :: min_ratio   ! The minimum ratio of the values of zw used to interpolate the edge values [nondim]
+  real :: wt1         ! The weight of the upper layer in the interpolated shared edge value [nondim]
   real :: slope_edge(nk+1)  ! Tracer slopes at the edges [A H-1 ~> A m-1 or A kg m-2]
   real :: val_edge(nk+1)    ! A weighted average edge concentration [A]
   integer :: i, k
+
+  min_ratio = 1.0e-8
 
   ! The WENO remapper is not currently written to work with massless layers, so set
   ! the thicknesses for very thin layers to some minimum value.
@@ -687,29 +697,27 @@ subroutine hybgen_weno_coefs(s, h_src, edges, nk, ns, thin, PCM_lay)
     zw(k,1) = 0.0
     zw(k,2) = 0.0
     do k=2,nk-1
-      if (PCM_layer(k)) then  !use PCM
+      if ((slope_edge(K)*slope_edge(K+1) < 0.0) .or. PCM_layer(k)) then  !use PCM
         edges(k,1,i) = s(k,i)
         edges(k,2,i) = s(k,i)
         zw(k,1) = 0.0
         zw(k,2) = 0.0
       else
-        q001 = dp(k)*slope_edge(K+1)
-        q002 = dp(k)*slope_edge(K)
-        if (q001*q002 < 0.0) then
-          q001 = 0.0
-          q002 = 0.0
-        endif
+        seh1 = dp(k)*slope_edge(K+1)
+        seh2 = dp(k)*slope_edge(K)
         q01 = dpkm2kp(k)*slope_edge(K+1)
         q02 = dpkm2kp(k)*slope_edge(K)
-        if (abs(q001) > abs(q02)) then
-          q001 = q02
+        if (abs(seh1) > abs(q02)) then
+          seh1 = q02
         endif
-        if (abs(q002) > abs(q01)) then
-          q002 = q01
+        if (abs(seh2) > abs(q01)) then
+          seh2 = q01
         endif
-        curv_cell = (q001 - q002) * qdpkmkp(k)
-        q001 = q001 - curv_cell*dp(k+1)
-        q002 = q002 + curv_cell*dp(k-1)
+        curv_cell = (seh1 - seh2) * qdpkmkp(k)
+        q001 = seh1 - curv_cell*dp(k+1)
+        q002 = seh2 + curv_cell*dp(k-1)
+        ! q001 = (seh1 * (dp(k-1) + dp(k)) + seh2 * dp(k+1)) * qdpkmkp(k)
+        ! q002 = (seh2 * (dp(k+1) + dp(k)) + seh1 * dp(k-1)) * qdpkmkp(k)
 
         edges(k,2,i) = s(k,i) + q001
         edges(k,1,i) = s(k,i) - q002
@@ -724,26 +732,22 @@ subroutine hybgen_weno_coefs(s, h_src, edges, nk, ns, thin, PCM_lay)
     zw(k,  2) = 0.0
 
     do k=2,nk
-      ds2a = max(zw(k-1,2), dsmll)
-      ds2b = max(zw(k,  1), dsmll)
-      val_edge(K) = (ds2b*edges(k-1,2,i)+ds2a*edges(k,1,i)) / (ds2b+ds2a)
-      ! ###RWH: To avoid introducing a dimensional parameter dsmll, consider the following:
-      !   if (zw(k,1) + zw(k-1,2) > 0.0) then
-      !     val_edge(k) = (zw(k,1)*edges(k-1,2,i) + zw(k-1,2)*edges(k,1,i)) / (zw(k,1) + zw(k-1,2))
-      !   else
-      !     val_edge(k) = 0.5*(edges(k-1,2,i) + edges(k,1,i))
-      !   endif
-      ! Or else perhaps introduce a parameter like min_ratio
-      !   if (zw(k,1) + zw(k-1,2) <= 0.0) then
-      !     wt1 = 0.5
-      !   elseif (zw(k,1) <= min_ratio * (zw(k,1) + zw(k-1,2))) then
-      !     wt1 = min_ratio
-      !   elseif (zw(k-1,2) <= min_ratio * (zw(k,1) + zw(k-1,2)))
-      !     wt1 = (1.0 - min_ratio)
-      !   else
-      !     wt1 = zw(k,1) / (zw(k,1) + zw(k-1,2))
-      !   endif
-      !   val_edge(k) = wt1*edges(k-1,2,i) + (1.0-wt1)*edges(k,1,i))
+      ! This was the original code based on that in Hycom, but because zw has
+      ! dimensions of [A2], it can not use a constant (hard coded) value of dsmll.
+      !   ds2a = max(zw(k-1,2), dsmll)
+      !   ds2b = max(zw(k,  1), dsmll)
+      !   val_edge(K) = (ds2b*edges(k-1,2,i)+ds2a*edges(k,1,i)) / (ds2b+ds2a)
+      ! Use a weighted average of the two layers' estimated edge values as the actual edge value.
+      if (zw(k,1) + zw(k-1,2) <= 0.0) then
+        wt1 = 0.5
+      elseif (zw(k,1) <= min_ratio * (zw(k,1) + zw(k-1,2))) then
+        wt1 = min_ratio
+      elseif (zw(k-1,2) <= min_ratio * (zw(k,1) + zw(k-1,2))) then
+        wt1 = (1.0 - min_ratio)
+      else
+        wt1 = zw(k,1) / (zw(k,1) + zw(k-1,2))
+      endif
+      val_edge(k) = wt1*edges(k-1,2,i) + (1.0-wt1)*edges(k,1,i)
     enddo !k
     val_edge(   1) = 2.0*s( 1,i)-val_edge( 2)  !not used?
     val_edge(nk+1) = 2.0*s(nk,i)-val_edge(nk)  !not used?
